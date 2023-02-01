568b8ba88a59ad1c701180f50526846b6ea9768a
Artem Gusev <artemgusev.dev@gmail.com>
2023-02-01 12:52:07 +0300

    Separated the list of expenses and improved the logic of conditional content lecture

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [selectedFilter, setSelectedFilter] = useState('2023');
@@ -15,19 +15,6 @@ const Expenses = (props) => {
     return item.date.getFullYear().toString() === selectedFilter;
   });
 
-  let expensesContent = <p>No expenses found.</p>;
-  
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map(expense => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        date={expense.date}
-        amount={expense.amount}
-      />
-    ));
-  }
-
   return (
     <div>
       <Card className='expenses'>
@@ -35,7 +22,7 @@ const Expenses = (props) => {
           onFilterChanged={onFilterChangedHandler} 
           defaultFilter={selectedFilter}
         />
-        {expensesContent}
+        <ExpensesList items={filteredExpenses}/>
       </Card>
     </div>
   );
